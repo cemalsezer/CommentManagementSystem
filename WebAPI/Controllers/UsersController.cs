afaae1f1d7d496b325110e21c65860d6ea17f9a6
CemalSezer <infocemalsezer@gmail.com>
2024-06-08 01:27:50 +0300

    WebAPI layer is added

---
+++ WebAPI/Controllers/UsersController.cs
@@ -0,0 +1,24 @@
+﻿using Business.Abstracts;
+using Business.Dtos.User.Requests;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public string Test()
+        {
+            return "HELLO";
+        }
+       
+
+
+
+
+    }
+}
