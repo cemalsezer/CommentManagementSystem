34105c259afe0886dc54af15efcc01a1ed6e71b8
CemalSezer <infocemalsezer@gmail.com>
2024-06-07 18:27:31 +0300

    Dtos are added on Business layer

---
+++ Business/Dtos/Assignment/Responses/GetListAssignmentResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Dtos.Assignment.Responses
+{
+    public class GetListAssignmentResponse
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+}
